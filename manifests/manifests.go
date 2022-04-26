c9ead0c25987a72987ea30d94c3bbbe28c503ac2
pbochynski <p.bochynski@gmail.com>
2022-04-26 15:21:24 +0200

    Fix rendering charts

--- manifests/manifests.go
+++ manifests/manifests.go
@@ -8,6 +8,9 @@ import (
 // TODO: _helpers.tpl files are not taken without specific templates. See: https://github.com/golang/go/issues/43854 resolved in go 1.18.1
 //
 //go:embed charts/* crds components.yaml
-//go:embed charts/*/*/_*.tpl
-//go:embed charts/*/*/*/*/_*.tpl
+//go:embed charts/*/*/_*
+//go:embed charts/*/*/*/_*
+//go:embed charts/*/*/*/*/_*
+//go:embed charts/*/*/*/*/*/_*
+//go:embed charts/*/*/*/*/*/*/_*
 var FS embed.FS
--- pkg/helm/renderer_test.go
+++ pkg/helm/renderer_test.go
@@ -1,29 +1,54 @@
 package helm
 
 import (
+	"io/fs"
 	"testing"
 
 	"github.com/kyma-incubator/kymactl/manifests"
+	"gopkg.in/yaml.v2"
 )
 
 func TestAllChartsCanBeRendered(t *testing.T) {
 
-	r := NewGenericRenderer(manifests.FS, "charts/serverless", "serverless", "kyma-system")
-
-	err := r.Run()
+	components := map[interface{}]interface{}{}
+	by, err := fs.ReadFile(manifests.FS, "components.yaml")
 	if err != nil {
-		t.Error(err)
+		t.Errorf("Cannot read component.yaml: %s", err)
 	}
-	evaluation, err := LoadValues("evaluation", "charts/serverless")
-	if err != nil {
-		t.Error(err)
+	var list []interface{}
+	yaml.Unmarshal(by, &components)
+	for _, c := range components["components"].([]interface{}) {
+		list = append(list, c)
 	}
-	t.Logf("Evaluation profile: %s", evaluation)
+	for _, c := range components["prerequisites"].([]interface{}) {
+		list = append(list, c)
+	}
 
-	evaluationManifest, err := r.RenderManifest(evaluation)
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("Evaluation manifest:\n%s", evaluationManifest)
+	for _, c := range list {
+		var component = c.(map[interface{}]interface{})
 
+		name := component["name"].(string)
+		namespace := "kyma-system"
+		if component["namespace"] != nil {
+			namespace = component["namespace"].(string)
+		}
+
+		t.Logf("Component %s (%s)", name, namespace)
+		r := NewGenericRenderer(manifests.FS, "charts/"+name, name, namespace)
+
+		err = r.Run()
+		if err != nil {
+			t.Error(err)
+		}
+		evaluation, err := LoadValues("evaluation", "charts/"+name)
+		if err != nil {
+			t.Logf("No evaluation profile for %s", name)
+		}
+
+		_, err = r.RenderManifest(evaluation)
+		if err != nil {
+			t.Error(err)
+		}
+
+	}
 }
